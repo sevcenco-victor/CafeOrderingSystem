eedca89fc7d1891759c8d60ac98108506c12e96e
ritzu <sevcenco.dev@gmail.com>
2025-02-14 09:32:55 +0200

    refactor(Ingredient): Add price constraint, override ToString, Equals, GetHashCode methods, made name readonly.

--- CafeOrderingSystem/Models/Ingredient.cs
+++ CafeOrderingSystem/Models/Ingredient.cs
@@ -2,16 +2,48 @@ namespace CafeOrderingSystem.Models;
 
 public class Ingredient
 {
-    public string Name { get; set; }
-    public decimal Price { get; set; }
+    public string Name { get; }
 
-    public Ingredient()
+    private decimal _price;
+
+    public decimal Price
     {
+        get => _price;
+        set
+        {
+            if (value < 0M)
+            {
+                throw new ArgumentException("Price must be greater then or equal to zero");
+            }
+
+            _price = value;
+        }
     }
 
     public Ingredient(string name, decimal price)
     {
         Name = name;
         Price = price;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Ingredient ingredient)
+        {
+            return Name.Equals(ingredient.Name)
+                   && Price == ingredient.Price;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name);
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} -> {Price:C}";
     }
 }
